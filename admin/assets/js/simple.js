35ba90260ded04d41781aed186530a0abc2f3295
bngesp <bassiroungom26@gmail.com>
2015-10-10 01:22:26 +0000

    dossier assets pour le js, css, image et le font

---
+++ admin/assets/js/simple.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    //themes, change CSS with JS
+    //default theme(CSS) is cerulean, change it if needed
+    
+
+    $('.navbar-toggle').click(function (e) {
+        e.preventDefault();
+        $('.nav-sm').html($('.navbar-collapse').html());
+        $('.sidebar-nav').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+
+    var $sidebarNav = $('.sidebar-nav');
+
+    // Hide responsive navbar on clicking outside
+    $(document).mouseup(function (e) {
+        if (!$sidebarNav.is(e.target) // if the target of the click isn't the container...
+            && $sidebarNav.has(e.target).length === 0
+            && !$('.navbar-toggle').is(e.target)
+            && $('.navbar-toggle').has(e.target).length === 0
+            && $sidebarNav.hasClass('active')
+            )// ... nor a descendant of the container
+        {
+            e.stopPropagation();
+            $('.navbar-toggle').click();
+        }
+    });
+
+
+    //highlight current / active link
+    $('ul.main-menu li a').each(function () {
+        if ($($(this))[0].href == String(window.location))
+            $(this).parent().addClass('active');
+    });
+
+
+    $('.accordion > a').click(function (e) {
+        e.preventDefault();
+        var $ul = $(this).siblings('ul');
+        var $li = $(this).parent();
+        if ($ul.is(':visible')) $li.removeClass('active');
+        else                    $li.addClass('active');
+        $ul.slideToggle();
+    });
+
+    $('.accordion li.active:first').parents('ul').slideDown();
+
+
+    //other things to do on document ready, separated for ajax calls
+    
+});
